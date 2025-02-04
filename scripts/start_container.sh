464aa83d819065354df46ee34a89306c81f56a79
Owais84 <163842309+Owais84@users.noreply.github.com>
2025-02-04 23:26:13 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
   set -e
 
   # Pull the latest Docker image from Docker Hub
-  docker pull trainwithshubham/tws-aws-cicd-demo:latest
+  docker pull owaiskhan84/tws-aws-cicd-demo:latest
 
   # Run the Docker image as a container
-  docker run -d -p 8000:8000 trainwithshubham/tws-aws-cicd-demo:latest
+  docker run -d -p 8000:8000 owaiskhan84/tws-aws-cicd-demo:latest
